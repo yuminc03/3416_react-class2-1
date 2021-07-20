bda31f78b8c8635170c830fa704db401a2971227
yuminc03 <yuminc03@gamil.com>
2021-07-20 16:01:08 +0900

    2021-07-20: 왼쪽버튼을 누르면 숫자가 커지고 오른쪽버튼을 누르면 숫자가 작아지는 프로젝트

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Counter from './Counter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+  return <Counter/>;
 }
 
 export default App;
+
+
+// //클래스 컴포넌트
+// import React, {Component} from 'react';
+// import './App.css';
+
+// class App extends Component{
+//   render(){
+//     return (
+//       <div>Component</div>
+//     )
+//   }
+// };
+
+// export default App;
